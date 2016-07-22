7a4dee9aed8a10fdb99295178b3a4adf599df4b5
Gustavo Solaira <gustavos@codeaurora.org>
2016-07-22 09:24:54 -0700

    Revert "open-avb: Change the socket type to simple raw socket"
    This reverts commit 0c782eb268738d1fffa446b9fe8619d31edede97
    
    Change-Id: If4f2cc8d067facca6f739bcfae11f270ee7e670f

--- lib/avtp_pipeline/platform/Linux/rawsock/openavb_rawsock.c
+++ lib/avtp_pipeline/platform/Linux/rawsock/openavb_rawsock.c
@@ -73,7 +73,7 @@ void *openavbRawsockOpen(const char *ifname_uri, bool rx_mode, bool tx_mode, U16
 	AVB_TRACE_ENTRY(AVB_TRACE_RAWSOCK);
 
 	const char* ifname = ifname_uri;
-	char proto[IF_NAMESIZE] = "simple";
+	char proto[IF_NAMESIZE] = "ring";
 	char *colon = strchr(ifname_uri, ':');
 	if (colon) {
 		ifname = colon + 1;
