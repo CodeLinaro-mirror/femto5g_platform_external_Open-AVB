589861acd8fa8f0b80eec6ad5c130bab510480a6
Prasanna Kumar <kprasan@codeaurora.org>
2019-07-02 20:22:11 +0530

    open-avb: Remove debug flag from libgptp test
    Change-Id: Id204257e2c340ccb080bb3edd6339dc1bfe35e28

--- examples/libgptp_test/Android.mk
+++ examples/libgptp_test/Android.mk
@@ -9,7 +9,7 @@ LOCAL_MODULE:= libgptp_test
 
 LOCAL_C_INCLUDES:= external/open-avb/lib/libgptp/
 LOCAL_SHARED_LIBRARIES:= libgptp
-LOCAL_MODULE_TAGS := optional debug
+LOCAL_MODULE_TAGS := optional
 LOCAL_CFLAGS := -Wno-unused-parameter -Wno-unused-variable
 
 LOCAL_SHARED_LIBRARIES += \
