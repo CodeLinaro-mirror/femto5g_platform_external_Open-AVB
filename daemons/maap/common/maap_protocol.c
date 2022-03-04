5731f267cee8a153a424914a5b3729d789acbbc3
Ian Wei <quic_ianwei@quicinc.com>
2022-03-04 10:54:10 +0800

    Open-avb: fix _FORTIFY_SOURCE build failure
    * build fail after enable security flags
    
    Change-Id: I1e79fa2dfa97dd5cf7b5288e124dc8cf58245dd2

--- daemons/maap/common/maap_protocol.c
+++ daemons/maap/common/maap_protocol.c
@@ -57,7 +57,7 @@ int state_transit(maap_info_t *maap_info)
 	uint8_t *s = NULL;
 	int i= 0;
 	int num_bytes = 0;
-	int maap_probe_count;
+	int maap_probe_count = MAAP_PROBE_RETRANSMITS;
 
 	switch (current_state) {
 	case INITIAL:
