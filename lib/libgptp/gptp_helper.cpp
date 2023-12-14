b029cd63388805866395de41b4e65b041d96ef14
Sandeep PR <quic_spr@quicinc.com>
2023-12-14 08:44:19 +0530

    open-avb: Changes to update the license for the qc changes
    * add the qc liceses for the updated changes of loadable lib
    
    Change-Id: I2cb2f9b1ea1b3af1fea3006f723659341e228e53

--- lib/libgptp/gptp_helper.cpp
+++ lib/libgptp/gptp_helper.cpp
@@ -54,6 +54,12 @@ Brush Technology and Ben Hoyt - Copyright (c) 2009, Brush Technology and Copyrig
 Complete license and copyright information can be found at
 https://github.com/benhoyt/inih/commit/74d2ca064fb293bc60a77b0bd068075b293cf175.
 ============================================================================ */
+/*
+Changes from Qualcomm Innovation Center are provided under the following license:
+Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
+SPDX-License-Identifier: BSD-3-Clause-Clear
+*/
+
 #include <linux/ptp_clock.h>
 #include <sys/mman.h>
 #include <sys/ioctl.h>
