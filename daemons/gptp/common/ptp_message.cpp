9e6099e5a7d8bc9d71e763f18d2d7c3aac51552c
Abdul Razak <quic_abduraza@quicinc.com>
2025-05-26 18:16:55 +0530

    qgptp: Added check for reverse sync, when both are slave & Added checks gptp public api's
    Change-Id: I3b5b7e913afe08b4c61cbd62378a2d23b78e4c9f

--- daemons/gptp/common/ptp_message.cpp
+++ daemons/gptp/common/ptp_message.cpp
@@ -564,6 +564,13 @@ PTPMessageCommon *buildPTPMessage
     memcpy(&(msg->domainNumber),
            buf + PTP_COMMON_HDR_DOMAIN_NUMBER(PTP_COMMON_HDR_OFFSET),
            sizeof(msg->domainNumber));
+    if ((msg->domainNumber == 1) && (port->getPortState() == PTP_SLAVE)) {
+        GPTP_LOG_LIMIT_ERROR(RX_TIMESTAMP_LOG,
+                "*** Received an reverse sync packet from slave to slave, discarding. domainNumber = %d port start = %d\n",
+                msg->domainNumber, port->getPortState());
+        goto abort;
+    }
+
     memcpy(&(msg->flags), buf + PTP_COMMON_HDR_FLAGS(PTP_COMMON_HDR_OFFSET),
            PTP_FLAGS_LENGTH);
     memcpy(&(msg->correctionField),
